0dfe4c8ac4629466329ac330ffa60a7d66cfa8f5
Lindsey Howard <miss_leading009@yahoo.com>
2022-01-30 11:32:01 -0500

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,8 @@ class Home extends Component {
   render() {
     return (
       <div className="main-content home">
-        <h2>About</h2>
+        <h2>City Tours App</h2>
+      <h3>Created by Lindsey</h3>
         <p>This fun directory is a project I made called <em>City Tours</em>.</p>
         <p>Lets explore different cities we can schedule to tour across the United States in some of the most aluring and iconic places.</p>
         <p>So let me show you some of the best things to do in the hottest cities that our country has to offer.</p>
