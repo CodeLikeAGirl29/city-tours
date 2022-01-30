5aff3f4db4c59717261eb590724595c38de08eef
Lindsey Howard <miss_leading009@yahoo.com>
2022-01-30 11:22:35 -0500

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,7 @@ class Home extends Component {
         <h2>About</h2>
         <p>This fun directory is a project I made called <em>City Tours</em>.</p>
         <p>Lets explore different cities we can schedule to tour across the United States in some of the most aluring and iconic places.</p>
-        <p>We have thousands of videos created by expert teachers on web design and front end development. Our library is continually refreshed with the latest on web technology so you will never fall behind.</p>
+        <p>So let me show you some of the best things to do in the hottest cities that our country has to offer.</p>
         <hr />
         
       </div>
