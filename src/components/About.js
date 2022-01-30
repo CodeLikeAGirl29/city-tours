c2b8b0ac394d640ca5cd0df48778772827578358
Lindsey Howard <miss_leading009@yahoo.com>
2022-01-30 11:30:37 -0500

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import city-tours-app from './data/city-tours-app.jpg';
+import city-tours-app from '../data/city-tours-app.jpg';
 
 const About = (props) => (
   <div className="main-content">
     <h2>{ props.title }</h2>
     <p>Travel is the main thing you purchase that makes you more extravagant”. We swear by this and put stock in satisfying travel dreams that make you perpetually rich constantly. We have been moving excellent encounters for a considerable length of time through our cutting-edge planned occasion bundles and other fundamental travel administrations. We rouse our clients to carry on with a rich life, brimming with extraordinary travel encounters.</p>
-<img src={city-tours-app} alt="my city tours logo" />
+<img src={require('../data/city-tours-app')} alt="my city tours logo" />
   </div>
 );
 
